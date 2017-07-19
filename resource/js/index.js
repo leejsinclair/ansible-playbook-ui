79cfa43b1c20334cd84d2f38f191be4b32344efe
leejsinclair <lee.j.sinclair@gmail.com>
2017-07-19 12:05:19 +1000

    GUI updates

--- resource/js/index.js
+++ resource/js/index.js
@@ -79,6 +79,9 @@ function parseYML(text) {
 function extractTags( doc ) {
     // Extract tags
     var tags = [];
+    if( !doc ) {
+        return [];
+    }
     doc.forEach(function(d) {
         if( d.tasks ) {
             var tasks = d.tasks;
@@ -97,11 +100,37 @@ sse.addEventListener('message', function(e) {
     // console.log(e.data);
     var refilename = recomma(running);
     $('#li' + refilename).click();
-    var content = localStorage.getItem(refilename) + '<p>'+e.data + '</p>';
+    var content = localStorage.getItem(refilename) + e.data + '\n';
     localStorage.setItem(refilename, content);
+
+    var hasIssue = getIssue(e.data);
 
     $('#content' + refilename).html(content);
 }, false);
+
+function getIssue( str ) {
+    var finalLineContains = [ ':', 'RECAP' ];
+    var line = _.unique(str.split(' '));
+    var isFinalLine = _.intersection( line, finalLineContains ).length===finalLineContains.length;
+
+    if( isFinalLine ) {
+        if( str.indexOf('failed=0') <0 || str.indexOf('unreachable=0')<0 ) {
+            console.error('ERROR');
+            beep();
+        }
+    }
+}
+
+/**
+ * @func beep
+ * @name beep
+ * @memberof app.betia.arbControl
+ * @description Play sound in browser. This function will play a sound as long as a sound has not been play in the last 500 ms
+ */
+function beep() {
+    var snd = new Audio("data:audio/wav;base64,/+MYxAAKsAaxmUAQAqqq5jfsQAgsEC4P6ji4Pg+oMFHSjgQ/yH4nef//KHP/w//nP/UGJQEHf6k2dEkT6bQ3Dc3nEyWLv+cR/+MYxBAQciqwAY1oAC8Z/5WNBR6gJr6wHN9S/f//x6DqvdmeI4df8Rwv//UaJpIqV+PL/48frT+v9f6v9f+pCw+LhwSEgGEf/+MYxAkPGpLoAYVQABRjw6Uxv838wlY895xqHBWkvOH728Ov+H/9H/V0f80eI/8PDP/j1v05f////8jd9Dv//Wpy27E04PuT/+MYxAcO02L2Oco4AzMb05pswtfoy/S/OcFTf/FcSjv///xULn///84eAPOO/+3/////9B4Cxv///////+PA+NxqJjgQjhtH/+MYxAYOsoq6WVBoANEmUWt9SWcNavf/9RRDjTH/oIgEYbh52sv//9ZLBxf///k8Lwi3/qKf/////9SYwRL///+igjjvX25l/+MYxAYNyhqYIYKYANP//zgk9ZgDXCyIaAW2m+gLUJ9Nv03/Nv4/j1+wfmWT/6xaTr/zUiTaH89/89/oO/VVFEDgLP+sRHsr/+MYxAkLUAZ2GcAQAP4sDXzsQgqCp3/WCv/1A0DP+z//1nf8JYiBoFQVBUFQkDQNB1VMQU1FMy45OS41VVVVVVVVVVVVVVVV/+MYxBYAAANIAAAAAFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV");
+    snd.play();
+}
 
 function recomma(text) {
     return text.replace('.', '_')
@@ -128,5 +157,5 @@ $filecontent.children().each(function(i,e) {
     var e = $(e);
     var key = e.data('rename');
     e.html(localStorage.getItem(key));
-})
+});
 $filehead.children(':first').children().click();
